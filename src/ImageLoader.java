5afee6caeda8d5dc0921947f310ed03189f1ac6a
4rne <maps.arne@googlemail.com>
2018-06-08 19:28:29 +0200

    pull class apart

---
+++ src/ImageLoader.java
@@ -0,0 +1,59 @@
+import java.awt.image.BufferedImage;
+import java.awt.image.DataBufferByte;
+import java.io.File;
+import java.io.IOException;
+
+import javax.imageio.ImageIO;
+
+public class ImageLoader
+{
+	public static int[][] readImage(String path)
+	{
+		try
+		{
+			BufferedImage img = ImageIO.read(new File(path));
+			int width;
+			int height;
+			boolean hasAlphaChannel;
+			int pixelLength;
+			byte[] pixels;
+
+			pixels = ((DataBufferByte) img.getRaster().getDataBuffer()).getData();
+			width = img.getWidth();
+			height = img.getHeight();
+			hasAlphaChannel = img.getAlphaRaster() != null;
+			pixelLength = 3;
+			if (hasAlphaChannel)
+			{
+				pixelLength = 4;
+			}
+
+			int imgData[][] = new int[width][height];
+			for (int x = 0; x < width; x++)
+			{
+				for (int y = 0; y < height; y++)
+				{
+					int pos = (y * pixelLength * width) + (x * pixelLength);
+
+					int argb = -16777216; // 255 alpha
+					if (hasAlphaChannel)
+					{
+						argb = (((int) pixels[pos++] & 0xff) << 24); // alpha
+					}
+
+					argb += ((int) pixels[pos++] & 0xff); // blue
+					argb += (((int) pixels[pos++] & 0xff) << 8); // green
+					argb += (((int) pixels[pos++] & 0xff) << 16); // red
+					imgData[x][y] = argb;
+				}
+			}
+			return imgData;
+		} catch (IOException e)
+		{
+			System.err.println("Image could not be loaded");
+			e.printStackTrace();
+			System.exit(42);
+		}
+		return null;
+	}
+}
--- src/Main.java
+++ src/Main.java
@@ -1,16 +1,9 @@
-import java.awt.image.BufferedImage;
-import java.awt.image.DataBufferByte;
-import java.io.File;
-import java.io.FileReader;
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.net.Socket;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.SplittableRandom;
-
-import javax.imageio.ImageIO;
-import javax.imageio.stream.FileImageInputStream;
 
 class Main
 {
@@ -33,7 +26,8 @@ class Main
 		}
 		if (arguments.isEmpty())
 		{
-			System.err.println("Need to specify at least -ip and -port. -mode -dim x,y");
+			System.err.println(
+					"Need to specify at least -ip and -port. -mode [random, linear] -dim x,y -img src -scale [0..n]");
 			System.exit(1);
 		}
 		if (!arguments.containsKey("port") || !arguments.containsKey("ip"))
@@ -48,7 +42,7 @@ class Main
 		int x;
 		int y;
 
-		int[][] image = readImage("/home/arne/workspace/pixelflut/kliemann.jpg");
+		int[][] image = ImageLoader.readImage(arguments.get("img"));
 		int imageScale = 1;
 		int imageOffsetX = 0;
 		int imageOffsetY = 0;
@@ -130,53 +124,4 @@ class Main
 		return randomInt(to - from) + from;
 	}
 
-	public static int[][] readImage(String path)
-	{
-		try
-		{
-			BufferedImage img = ImageIO.read(new File(path));
-			int width;
-			int height;
-			boolean hasAlphaChannel;
-			int pixelLength;
-			byte[] pixels;
-
-			pixels = ((DataBufferByte) img.getRaster().getDataBuffer()).getData();
-			width = img.getWidth();
-			height = img.getHeight();
-			hasAlphaChannel = img.getAlphaRaster() != null;
-			pixelLength = 3;
-			if (hasAlphaChannel)
-			{
-				pixelLength = 4;
-			}
-
-			int imgData[][] = new int[width][height];
-			for (int x = 0; x < width; x++)
-			{
-				for (int y = 0; y < height; y++)
-				{
-					int pos = (y * pixelLength * width) + (x * pixelLength);
-
-					int argb = -16777216; // 255 alpha
-					if (hasAlphaChannel)
-					{
-						argb = (((int) pixels[pos++] & 0xff) << 24); // alpha
-					}
-
-					argb += ((int) pixels[pos++] & 0xff); // blue
-					argb += (((int) pixels[pos++] & 0xff) << 8); // green
-					argb += (((int) pixels[pos++] & 0xff) << 16); // red
-					imgData[x][y] = argb;
-				}
-			}
-			return imgData;
-		} catch (IOException e)
-		{
-			System.err.println("Image could not be loaded");
-			e.printStackTrace();
-			System.exit(42);
-		}
-		return null;
-	}
 }

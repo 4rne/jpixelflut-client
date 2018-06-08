d3724f1c61babbb8fb2a118169bf7de71e27fd6e
4rne <maps.arne@googlemail.com>
2018-06-08 19:14:41 +0200

    add parameters

--- src/Main.java
+++ src/Main.java
@@ -5,25 +5,44 @@ import java.io.FileReader;
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.net.Socket;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.SplittableRandom;
 
 import javax.imageio.ImageIO;
 import javax.imageio.stream.FileImageInputStream;
 
 class Main
-
 {
 	static int xMax = 1024;
 	static int yMax = 575;
 	static String lineEnding = "\n"; // or "\\n"
 	static PrintWriter out;
 	static SplittableRandom randomInstance;
+	private static Map<String, String> arguments = new HashMap<String, String>();
 
 	public static void main(String[] args) throws IOException
-
 	{
+		if (args.length > 0 && args.length % 2 == 0)
+		{
+			for (int i = 0; i < args.length; i += 2)
+			{
+				arguments.put(args[i].substring(1), args[i + 1]);
+			}
+			System.out.println(arguments.toString());
+		}
+		if (arguments.isEmpty())
+		{
+			System.err.println("Need to specify at least -ip and -port. -mode -dim x,y");
+			System.exit(1);
+		}
+		if (!arguments.containsKey("port") || !arguments.containsKey("ip"))
+		{
+			System.err.println("IP and/or port broken.");
+			System.exit(1);
+		}
 		@SuppressWarnings("resource")
-		Socket socket = new Socket("192.168.0.20", 1337);
+		Socket socket = new Socket(arguments.get("ip"), Integer.parseInt(arguments.get("port")));
 		out = new PrintWriter(socket.getOutputStream(), true);
 		randomInstance = new SplittableRandom();
 		int x;
@@ -34,36 +53,40 @@ class Main
 		int imageOffsetX = 0;
 		int imageOffsetY = 0;
 
-		while (true) {
-//			x = randomInt(xMax);
-//			y = randomInt(yMax);
-//			double clock = x / (float) xMax * (Math.PI * 2);
-//			int r = (int) ((Math.sin(clock)                   + 1) * 127);
-//			int g = (int) ((Math.sin(clock + Math.PI * 0.666) + 1) * 127);
-//			int b = (int) ((Math.sin(clock + Math.PI * 1.333) + 1) * 127);
-//			pixel(x, y, r, g, b);
-			
+		while (true)
+		{
+			// x = randomInt(xMax);
+			// y = randomInt(yMax);
+			// double clock = x / (float) xMax * (Math.PI * 2);
+			// int r = (int) ((Math.sin(clock) + 1) * 127);
+			// int g = (int) ((Math.sin(clock + Math.PI * 0.666) + 1) * 127);
+			// int b = (int) ((Math.sin(clock + Math.PI * 1.333) + 1) * 127);
+			// pixel(x, y, r, g, b);
+
 			for (x = 0; x < image.length; x++)
 			{
 				for (y = 0; y < image[0].length; y++)
 				{
-//			x = randomInt(image.length);
-//			y = randomInt(image[0].length);
-					for (int i = 1; i <= imageScale; i++) {
-						for (int j = 1; j<= imageScale; j++) {
-							pixel(imageOffsetX + x * imageScale + i, imageOffsetY + y * imageScale + j, image[x][y] >> 16 & 0xFF, image[x][y] >> 8 & 0xFF, image[x][y] >> 0 & 0xFF);
+					// x = randomInt(image.length);
+					// y = randomInt(image[0].length);
+					for (int i = 1; i <= imageScale; i++)
+					{
+						for (int j = 1; j <= imageScale; j++)
+						{
+							pixel(imageOffsetX + x * imageScale + i, imageOffsetY + y * imageScale + j,
+									image[x][y] >> 16 & 0xFF, image[x][y] >> 8 & 0xFF, image[x][y] >> 0 & 0xFF);
 						}
 					}
 				}
 			}
-//			for (x = 0; x < xMax; x++) {
-//				for (y = 0; y < yMax; y++) {
-//					//pixel(randomInt(xMax), randomInt(yMax), 246, 188, 180);
-//					//out.println ("PX " + (int) (Math.random() * xMax) + " " + (int)
-//					//(Math.random() * yMax) + " 00ff00\\n");
-//					pixel(x, y, 0, 0, 0);
-//				}
-//			}
+			// for (x = 0; x < xMax; x++) {
+			// for (y = 0; y < yMax; y++) {
+			// //pixel(randomInt(xMax), randomInt(yMax), 246, 188, 180);
+			// //out.println ("PX " + (int) (Math.random() * xMax) + " " + (int)
+			// //(Math.random() * yMax) + " 00ff00\\n");
+			// pixel(x, y, 0, 0, 0);
+			// }
+			// }
 
 		}
 
@@ -106,7 +129,7 @@ class Main
 	{
 		return randomInt(to - from) + from;
 	}
-	
+
 	public static int[][] readImage(String path)
 	{
 		try
@@ -117,39 +140,38 @@ class Main
 			boolean hasAlphaChannel;
 			int pixelLength;
 			byte[] pixels;
-			
+
 			pixels = ((DataBufferByte) img.getRaster().getDataBuffer()).getData();
-	        width = img.getWidth();
-	        height = img.getHeight();
-	        hasAlphaChannel = img.getAlphaRaster() != null;
-	        pixelLength = 3;
-	        if (hasAlphaChannel)
-	        {
-	            pixelLength = 4;
-	        }
-	        
-	        int imgData[][] = new int[width][height];
-	        for (int x = 0; x < width; x++)
-	        {
+			width = img.getWidth();
+			height = img.getHeight();
+			hasAlphaChannel = img.getAlphaRaster() != null;
+			pixelLength = 3;
+			if (hasAlphaChannel)
+			{
+				pixelLength = 4;
+			}
+
+			int imgData[][] = new int[width][height];
+			for (int x = 0; x < width; x++)
+			{
 				for (int y = 0; y < height; y++)
 				{
-			        int pos = (y * pixelLength * width) + (x * pixelLength);
-		
-			        int argb = -16777216; // 255 alpha
-			        if (hasAlphaChannel)
-			        {
-			            argb = (((int) pixels[pos++] & 0xff) << 24); // alpha
-			        }
-		
-			        argb += ((int) pixels[pos++] & 0xff); // blue
-			        argb += (((int) pixels[pos++] & 0xff) << 8); // green
-			        argb += (((int) pixels[pos++] & 0xff) << 16); // red
-			        imgData[x][y] = argb;
+					int pos = (y * pixelLength * width) + (x * pixelLength);
+
+					int argb = -16777216; // 255 alpha
+					if (hasAlphaChannel)
+					{
+						argb = (((int) pixels[pos++] & 0xff) << 24); // alpha
+					}
+
+					argb += ((int) pixels[pos++] & 0xff); // blue
+					argb += (((int) pixels[pos++] & 0xff) << 8); // green
+					argb += (((int) pixels[pos++] & 0xff) << 16); // red
+					imgData[x][y] = argb;
 				}
-	        }
-	        return imgData;
-		}
-		catch (IOException e)
+			}
+			return imgData;
+		} catch (IOException e)
 		{
 			System.err.println("Image could not be loaded");
 			e.printStackTrace();
@@ -157,5 +179,4 @@ class Main
 		}
 		return null;
 	}
-
 }

e4aaee4d25bec7723dcf6c9510722f89a6fd78fa
ThePseud0o <maps.arne@googlemail.com>
2018-06-18 17:11:27 +0200

    add rainbow runnable

---
+++ src/pixelflut/RainbowRunnable.java
@@ -0,0 +1,47 @@
+package pixelflut;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.net.Socket;
+import java.net.UnknownHostException;
+import java.util.SplittableRandom;
+
+public class RainbowRunnable extends PixelRunnable implements Runnable
+{
+	int xMax;
+	int yMax;
+	int thread_number;
+	int threads;
+	static PrintWriter out;
+
+	public RainbowRunnable(int thread_number, int threads, int xMax, int yMax,
+			String ip, int port) throws UnknownHostException, IOException
+	{
+		this.threads = threads;
+		this.thread_number = thread_number;
+		this.xMax = xMax;
+		this.yMax = yMax;
+		@SuppressWarnings("resource")
+		Socket socket = new Socket(ip, port);
+		out = new PrintWriter(socket.getOutputStream(), true);
+		randomInstance = new SplittableRandom();
+	}
+
+	public void run()
+	{
+		int x;
+		int y;
+		while (true)
+		{
+			x = randomInt(xMax);
+			y = randomInt(yMax);
+
+			double clock = x / (float) xMax * (Math.PI * 2);
+			int r = (int) ((Math.sin(clock) + 1) * 127);
+			int g = (int) ((Math.sin(clock + Math.PI * 0.666) + 1) * 127);
+			int b = (int) ((Math.sin(clock + Math.PI * 1.333) + 1) * 127);
+			String pixel = pixelToString(x, y, r, g, b);
+			out.write(pixel);
+		}
+	}
+}

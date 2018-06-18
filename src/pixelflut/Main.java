cb25ae1f34d758c27c5d3cb3998626311d15abf1
ThePseud0o <maps.arne@googlemail.com>
2018-06-18 17:12:43 +0200

    remove duplicate old code from main class

--- src/pixelflut/Main.java
+++ src/pixelflut/Main.java
@@ -104,40 +104,4 @@ class Main
 			}
 		}
 	}
-
-	public static void pixel(int x, int y, int r, int g, int b)
-	{
-		StringBuilder str = new StringBuilder();
-		str.append("PX ");
-		str.append(x);
-		str.append(' ');
-		str.append(y);
-		str.append(' ');
-		str.append(toHex(r));
-		str.append(toHex(g));
-		str.append(toHex(b));
-		str.append(lineEnding);
-		out.print(str);
-	}
-
-	public static String toHex(int n)
-	{
-		String value = Integer.toHexString(n);
-		if (value.length() < 2)
-		{
-			value = "0" + value;
-		}
-		return value;
-	}
-
-	public static int randomInt(int to)
-	{
-		return randomInstance.nextInt(to);
-	}
-
-	public static int randomInt(int from, int to)
-	{
-		return randomInt(to - from) + from;
-	}
-
 }
